c18794c9395ab53db4bed0e1ba0d9d5e2474264e
redfire411 <94802367+redfire411@users.noreply.github.com>
2021-11-21 13:22:00 -0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -298,8 +298,7 @@ export default class WearAHat {
         }
 
         // If the user is wearing a hat, destroy it.
-        if (this.attachedHats.has(userId)) this.attachedHats.get(userId).destroy();
-        this.attachedHats.delete(userId);
+        
 
         const hatRecord = this.HatDatabase[hatId];
 
